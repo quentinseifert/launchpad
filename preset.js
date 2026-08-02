cd88d57532cb6d30b2886b209ac6898965ae0d82
SophiePotts <88346945+SophiePotts@users.noreply.github.com>
2026-08-02 17:27:25 +0200

    Update preset.js

--- preset.js
+++ preset.js
@@ -59,14 +59,15 @@ const PRESET_RAW = {
   "6,0": { clip: "clips/Front_Right.wav",     farbe: "pink",   name: "Tom Hi"  },
   "6,1": { clip: "clips/Front_Left.wav",    farbe: "weiss",  name: "Tom Low" },
   "0,1": { clip: "clips/airhorn.mp3",    farbe: "pink",  name: "airhorn" },
-  
+
+  // TBS song clips
   "7,0": { clip: "clips/Lachen_short.wav",    farbe: "lila",  name: "lachen" },
   "7,1": { clip: "clips/Lachen_short2.wav",    farbe: "lila",  name: "lachen2" },
   "7,2": { clip: "clips/dokter1.wav",    farbe: "lila",  name: "dokter" },
-  "7,3": { clip: "clips/kein_bock.wav.mp3",    farbe: "lila",  name: "kb" },
-  "7,4": { clip: "clips/wieso.wav.mp3",    farbe: "lila",  name: "wieso" },
-  "7,5": { clip: "clips/arbeit.mp3",    farbe: "lila",  name: "arbeit" },
-  "7,6": { clip: "clips/cool.mp3",    farbe: "lila",  name: "cool" },
+  "7,3": { clip: "clips/kein_bock.wav",    farbe: "lila",  name: "kb" },
+  "7,4": { clip: "clips/wieso.wav",    farbe: "lila",  name: "wieso" },
+  "7,5": { clip: "clips/arbeit.wav",    farbe: "lila",  name: "arbeit" },
+  "7,6": { clip: "clips/cool.wav",    farbe: "lila",  name: "cool" },
 
   // "0,1": { clip: "clips/bass.wav",    farbe: "rot-dunkel", name: "Bass" },
 };
